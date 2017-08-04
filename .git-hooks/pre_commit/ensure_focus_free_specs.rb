7219fc99cbbfa11a57e1315c3136b28e0ec4c77c
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2017-08-04 21:45:08 +0300

    Code style fix

--- .git-hooks/pre_commit/ensure_focus_free_specs.rb
+++ .git-hooks/pre_commit/ensure_focus_free_specs.rb
@@ -1,5 +1,3 @@
-# Source https://github.com/hallucinations/overcommit-hooks/
-
 require 'rspec'
 
 module Overcommit
@@ -7,14 +5,12 @@ module Overcommit
     module PreCommit
       # NOTE: This makes use of many methods from RSpecs private API.
       class EnsureFocusFreeSpecs < Base
-        def spec_files
-          `git diff --cached --name-only --diff-filter=ACM spec`.split("\n")
-        end
-
         def configure_rspec(applicable_files)
           RSpec.configure do |config|
             config.inclusion_filter = :focus
-            config.files_or_directories_to_run = applicable_files.select { |fn| fn.end_with?('_spec.rb') }
+            config.files_or_directories_to_run = applicable_files.select do |fn|
+              fn.end_with?('_spec.rb')
+            end
             config.inclusion_filter.rules
             config.requires = %w[spec_helper rails_helper]
             config.load_spec_files
@@ -25,7 +21,9 @@ module Overcommit
           configure_rspec(applicable_files)
           return :pass if RSpec.world.example_count.zero?
 
-          files = RSpec.world.filtered_examples.reject { |_k, v| v.empty? }.keys.map(&:file_path).uniq
+          files = RSpec.world.filtered_examples
+                       .reject { |_k, v| v.empty? }
+                       .keys.map(&:file_path).uniq
 
           [:fail, "Trying to commit focused spec(s) in:\n\t#{files.join("\n\t")}"]
         end
